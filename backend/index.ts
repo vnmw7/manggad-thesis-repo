4db7f570bbf23f22d365b14cf39078db87d48284
gy10jv9 <146041946+gy10jv9@users.noreply.github.com>
2024-08-28 09:38:36 +0800

    initialized next.js

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response , Application } from 'express';
 
 const app: Application = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
