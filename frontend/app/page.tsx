30e94094b986c4a1bd7b3062e0fa6fa0dfad34d6
gy10jv9 <146041946+gy10jv9@users.noreply.github.com>
2024-09-17 05:35:20 +0800

    added theme

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,35 @@
+"use client"
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import ThemeSwitch from "./components/ThemeSwitch";
 
 export default function Home() {
+  const router = useRouter();
+
 	return (
-		<main>
-      <h1> Hello World! </h1>
-      <Link href="/auth/login">Login</Link>
-    </main>
+		<div>
+      {/* header and navigations */}
+      <header>
+          <div> logo here </div>
+          <ul>
+              <li> Home </li>
+              <li> Categories </li>
+              <li> Updates </li>
+          </ul>
+          <button onClick={() => router.push("/auth")}> Admin </button>
+      </header>
+
+      <h1> This is the Home Page </h1>
+      <div>
+          <input type="text" placeholder="Search Related Studies" />
+          <button >Search </button>
+      </div>
+
+      {/* footer */}
+      <footer>
+          <ThemeSwitch />
+      </footer>
+    </div>
 	);
 }
