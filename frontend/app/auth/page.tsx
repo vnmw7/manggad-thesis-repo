ce71149a8b8745359d21829e85023ac753d26ada
gy10jv9 <146041946+gy10jv9@users.noreply.github.com>
2024-09-17 05:56:49 +0800

    added login and registration page

--- frontend/app/auth/page.tsx
+++ frontend/app/auth/page.tsx
@@ -1,6 +1,17 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
 const AuthenticationPage = () => {
+  const router = useRouter()
+
   return (
-    <div> AuthenticationPage </div>
+    <div> 
+      <h1> You will be proceeding as admin.  </h1>
+      <button onClick={() => router.push("/auth/login")}> Login </button>
+      <button onClick={() => router.push("/auth/register")}> Register </button>
+      <button onClick={() => router.push("/")}> Back </button>
+    </div>
   )
 }
 
--- frontend/app/components/ThemeSwitch.tsx
+++ frontend/app/components/ThemeSwitch.tsx
@@ -8,9 +8,9 @@ export default function ThemeSwitch() {
   const [mounted, setMounted] = useState(false)
   const { setTheme, resolvedTheme } = useTheme()
 
-  useEffect(() =>  setMounted(true), [])
+  useEffect(() =>  setMounted(true), []) // to prevent rendering issues related to server-side rendering (SSR) in Next.js
 
-  if (!mounted) return (
+  if (!mounted) return ( // loading indicator lang na ang image mag switch theme
     <Image
       src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
       width={36}
@@ -22,12 +22,13 @@ export default function ThemeSwitch() {
     />
   )
 
-  if (resolvedTheme === 'dark') {
+  if (resolvedTheme === 'dark') { // para mag switch ang theme pati ang button
     return <button onClick={() => setTheme('light')}> Switch to Light Mode </button>
   }
 
-  if (resolvedTheme === 'light') {
-    return <button onClick={() => setTheme('dark')}> Switch to Dark Mode </button>
+  if (resolvedTheme === 'light') { // para mag switch ang theme pati ang button
+    return <button onClick={() => setTheme('dark')}> Switch to Light Mode </button>
   }
 
+  // source: https://www.davegray.codes/posts/light-dark-mode-nextjs-app-router-tailwind
 }
