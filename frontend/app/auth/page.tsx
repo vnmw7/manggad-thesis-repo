5b3ef88a6e33d6e2410ce4dab26d13a554e03375
ppgs <vincentm.work.2023@gmail.com>
2024-09-25 11:09:59 +0800

    added fonts

--- frontend/app/auth/page.tsx
+++ frontend/app/auth/page.tsx
@@ -3,16 +3,17 @@
 import { useRouter } from "next/navigation"
 
 const AuthenticationPage = () => {
-  const router = useRouter()
+    const router = useRouter()
 
-  return (
-    <div> 
-      <h1> You will be proceeding as admin.  </h1>
-      <button onClick={() => router.push("/auth/login")}> Login </button>
-      <button onClick={() => router.push("/auth/register")}> Register </button>
-      <button onClick={() => router.push("/")}> Back </button>
-    </div>
-  )
+    return (
+      <div> 
+        <h1> Admin Authentication </h1>
+        <p> You will be proceeding as an admin of this thesis repository. </p>
+        <button onClick={() => router.push("/auth/login")}> Login </button>
+        <button onClick={() => router.push("/auth/register")}> Register </button>
+        <button onClick={() => router.push("/")}> Back </button>
+      </div>
+    )
 }
 
 export default AuthenticationPage
