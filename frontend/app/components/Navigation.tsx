037af31ff914f934b1fc8c67c818b9e5495a421e
Jexaruuu <barsanajexter@gmail.com>
2024-09-30 15:33:10 +0800

    last commit

--- frontend/app/components/Navigation.tsx
+++ frontend/app/components/Navigation.tsx
@@ -4,15 +4,17 @@ const Navigation = () => {
     const router = useRouter();
 
     return (
-        <div className="bg-[#a5a29d]">
-            <div> logo here </div>
-            <ul>
-                <li> Home </li>
-                <li> Categories </li>
-                <li> Updates </li>
+        <div className="bg-[#a5a29d] flex flex-col items-center">
+            <div className="mb-4"> {/* Margin for spacing */}
+                <img src="/path/to/logo.png" alt="Logo" />
+            </div>
+            <ul className="flex space-x-4"> {/* Adjust spacing as needed */}
+                <li>Home</li>
+                <li>Categories</li>
+                <li>Updates</li>
             </ul>
         </div>  
-    )
+    );
 }
 
-export default Navigation
+export default Navigation;
