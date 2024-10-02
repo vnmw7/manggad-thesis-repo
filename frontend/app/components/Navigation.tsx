23ef2e8dc8a2954db72e904a531f8a96cf9f989a
ppgs <vincentm.work.2023@gmail.com>
2024-10-02 12:49:48 +0800

    fixed image and icon problem

--- frontend/app/components/Navigation.tsx
+++ frontend/app/components/Navigation.tsx
@@ -1,19 +1,17 @@
+"use client"
+
 import Image from "next/image";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faTh, faArrowUp, faCog } from '@fortawesome/free-solid-svg-icons';
+// import logo from "/logo.png";
 
 const Navigation = () => {
     return (
         <div className="bg-[#C1BEAF] w-20 h-screen border-r border-r-[#262832] border-transparent flex flex-col relative">
             {/* Logo Container */}
             <div className="mb-4 flex justify-center mt-1">
-                <Image 
-                    src="/lccb.png" 
-                    alt="Logo" 
-                    width={400}
-                    height={400}
-                    className="object-contain max-w-full" 
-                />
+            <div style={{ backgroundImage: 'url("lccb.png")', aspectRatio: "1/1", height: "100px", backgroundSize: "contain" }}>
+            </div>
             </div>
             {/* Navigation Items */}
             <ul className="flex flex-col items-center mt-4 space-y-4">
