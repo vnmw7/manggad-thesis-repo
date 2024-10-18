03345b30c7211acae2d8927c22c4d5a802889d82
vincentm.work.2023@gmail.com <vincentm.work.2023@gmail.com>
2024-10-18 19:10:38 +0800

    reorganized the file structure

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
-import "./globals.css";
-import ThemedElement from "./components/theme/ThemedElement";
+import "../styles/globals.css";
+import ThemedElement from "../components/theme/ThemedElement";
 
 export default function RootLayout({
 	children,
@@ -8,9 +8,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" suppressHydrationWarning>
-		<body>
-			<ThemedElement> {children} </ThemedElement>
-		</body>
+			<body>
+				<ThemedElement> {children} </ThemedElement>
+			</body>
 		</html>
 	);
 }
