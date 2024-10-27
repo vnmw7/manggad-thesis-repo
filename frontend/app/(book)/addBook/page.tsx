accb7c748941ef3d0bebadffb9b3394b64bc4b78
gy10jv9 <146041946+gy10jv9@users.noreply.github.com>
2024-10-27 17:15:07 +0800

    added navigation

---
+++ frontend/app/(book)/addBook/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+const AddBook = () => {
+	return (
+		<div>
+			<div> this is the header </div>
+			<div className="bg-gray-400 fixed z-10 top-0 left-0 w-[80vw] h-full">
+				<div> nav button </div>
+				<div> navigations </div>
+			</div>
+			<div>
+				form container
+				<div> this is the form </div>
+			</div>
+		</div>
+	)
+}
+
+export default AddBook
