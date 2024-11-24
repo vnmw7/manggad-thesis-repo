e6f208aaa7e67a27a09566c7401081f342e8f635
gy10jv9 <146041946+gy10jv9@users.noreply.github.com>
2024-11-24 15:15:46 +0800

    added note when starting the backend

--- backend/routes/bookRoutes.ts
+++ backend/routes/bookRoutes.ts
@@ -8,6 +8,6 @@ router.post('/', bookController.addBook);
 router.get('/view/:id', bookController.getBookById);
 router.delete('/delete/:id', bookController.deleteBookById);
 router.post('/edit/:id', bookController.editBookById);
-router.post('/search/', bookController.searchBooks);
+router.get('/search/', bookController.searchBooks);
 
 export default router;
