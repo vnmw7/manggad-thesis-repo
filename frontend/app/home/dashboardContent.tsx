2f5564f02c99ee8eb195c7fcb0780b2520dffd0a
Vincent Manalo <146041946+vnmw7@users.noreply.github.com>
2025-10-09 15:53:00 +0800

    feat: Remove unused FaPlus icon component from DashboardContent

--- frontend/app/home/dashboardContent.tsx
+++ frontend/app/home/dashboardContent.tsx
@@ -643,18 +643,6 @@ export default function DashboardContent() {
   );
 }
 
-// Additional icons for quick actions
-const FaPlus = ({ className }: { className?: string }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 448 512"
-    className={className}
-    fill="currentColor"
-  >
-    <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-  </svg>
-);
-
 const FaBookOpen = ({ className }: { className?: string }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
